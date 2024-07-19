c771bbd4731dbd74a67440831b17b37b5a6c9e6f
pranali2606 <[2022bit033@sggs.ac.in]>
2024-07-19 22:27:27 +0530

    second code

---
+++ is.py
@@ -0,0 +1,15 @@
+a=25
+b=25
+if(a is b) :
+ print("a and b are same")
+else:
+ print("a and b are not same")
+
+i = 20
+if (i is not 15):
+    print("i is smaller than 15")
+    print("i'm in if Block")
+else:
+    print("i is greater than 15")
+    print("i'm in else Block")
+print("i'm not in if and not in else Block")
